8ae16436b48ac2ea3d7c352fac99adef2c0fc458
J4gm <jack.a.g.mullin@outlook.com>
2025-04-15 15:54:50 +0100

    Allow kiwis to spawn in dark forests.

--- fabric/src/main/java/jagm/jagmkiwis/FabricEntrypoint.java
+++ fabric/src/main/java/jagm/jagmkiwis/FabricEntrypoint.java
@@ -26,7 +26,7 @@ public class FabricEntrypoint implements ModInitializer {
         Registry.register(BuiltInRegistries.ENTITY_TYPE, ResourceLocation.fromNamespaceAndPath(KiwiMod.MOD_ID, KiwiModEntities.LASER_BEAM_NAME), KiwiModEntities.LASER_BEAM.get());
         KiwiModSounds.SOUNDS_COMMON.forEach((name, soundSupplier) -> Registry.register(BuiltInRegistries.SOUND_EVENT, ResourceLocation.fromNamespaceAndPath(KiwiMod.MOD_ID, name), soundSupplier.get()));
         FabricDefaultAttributeRegistry.register(KiwiModEntities.KIWI.get(), KiwiEntity.createAttributes());
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(Biomes.FOREST, Biomes.FLOWER_FOREST, Biomes.BIRCH_FOREST, Biomes.OLD_GROWTH_BIRCH_FOREST, Biomes.CHERRY_GROVE), MobCategory.CREATURE, KiwiModEntities.KIWI.get(), 10, 3, 4);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(Biomes.FOREST, Biomes.FLOWER_FOREST, Biomes.BIRCH_FOREST, Biomes.OLD_GROWTH_BIRCH_FOREST, Biomes.CHERRY_GROVE, Biomes.DARK_FOREST), MobCategory.CREATURE, KiwiModEntities.KIWI.get(), 10, 3, 4);
         SpawnPlacements.register(KiwiModEntities.KIWI.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules);
         ServerEntityEvents.ENTITY_LOAD.register((entity, serverLevel) -> {
             if(entity instanceof Cat cat){
